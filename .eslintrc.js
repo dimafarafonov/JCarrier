37afcdab6735ca5dde401355efb3a9658749171c
d.farafonov <d.farafonov@omo.systems>
2022-03-08 23:12:00 +0200

    set up eslint, init RN project

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  plugins: ['react', 'react-native'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    'react-native/react-native': true,
+  },
+  rules: {
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react-native/no-inline-styles': 2,
+    'react-native/no-color-literals': 2,
+  },
+};
